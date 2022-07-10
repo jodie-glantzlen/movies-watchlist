2852ce75908c10858c24defe4530ee53faa662a2
jodie-glantzlen <jodie.glantzlen@gmail.com>
2022-07-11 09:45:53 +1200

    Create DB and seeds

---
+++ server/db/migrations/20220710210946_movies.js
@@ -0,0 +1,21 @@
+/**
+ * @param { import("knex").Knex } knex
+ * @returns { Promise<void> }
+ */
+exports.up = function(knex) {
+  return knex.schema.createTable('movies', (table) => {
+    table.increments('id')
+    table.string('title')
+    table.string('img')
+    table.string('imdb_id')
+    table.boolean('watched').defaultsTo(false)
+  })
+};
+
+/**
+ * @param { import("knex").Knex } knex
+ * @returns { Promise<void> }
+ */
+exports.down = function(knex) {
+  return knex.schema.dropTable('movies')
+};
